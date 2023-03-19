13e417b1e69168ea46d08e31ba55d6f3a69e2038
Daniel <daniel.jank@protonmail.com>
2023-03-19 22:27:48 +0100

    Add lights & floor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import * as lil from 'lil-gui'
+// import * as CANNON from 'cannon-es'
 
 
 // Canvas & resizing
@@ -18,21 +20,46 @@ window.addEventListener('resize', () => {
 })
 
 
+// Debug
+const gui = new lil.GUI()
+
+
 ////////////////////////////// Code //////////////////////////////
 
 
 // Scene & camera
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 2
+camera.position.y = 1
 camera.position.z = 3
 scene.add(camera)
 
 
 // Geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.y = 0.5
 scene.add(mesh)
+
+const plane = new THREE.PlaneGeometry(5, 5)
+const gmaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 })
+const gmesh = new THREE.Mesh(plane, gmaterial)
+gmesh.rotation.x = - Math.PI * 0.5
+gmesh.position.y = 0
+scene.add(gmesh)
+
+
+// Lights
+const ambientLight = new THREE.AmbientLight()
+ambientLight.color = new THREE.Color(0xffffff)
+ambientLight.intensity = 0.25
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 1, 15)
+pointLight.position.set(3, 3, -1)
+scene.add(pointLight)
 
 
 // Orbit controls
